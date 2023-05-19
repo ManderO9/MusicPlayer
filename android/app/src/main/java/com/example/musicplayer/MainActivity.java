2ce79953e16447197c5391e1cce61bb6574a05dd
ManderO9 <hossem.chetoui@gmail.com>
2023-05-19 18:32:12 +0100

    displaying songs available in the download directory

--- android/app/src/main/java/com/example/musicplayer/MainActivity.java
+++ android/app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -2,5 +2,37 @@ package com.example.musicplayer;
 
 import io.flutter.embedding.android.FlutterActivity;
 
+import androidx.core.app.ActivityCompat;
+
+import android.content.pm.PackageManager;
+
+import android.Manifest;
+import android.os.Bundle;
+
 public class MainActivity extends FlutterActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        this.RequestPermissions();
+        super.onCreate(savedInstanceState);
+    }
+
+
+    public void RequestPermissions() {
+        int check = ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.INTERNET);
+        if (check != PackageManager.PERMISSION_GRANTED)
+            requestPermissions(new String[]{Manifest.permission.INTERNET}, 1024);
+
+
+        check = ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (check != PackageManager.PERMISSION_GRANTED)
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1024);
+
+        check = ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (check != PackageManager.PERMISSION_GRANTED)
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1024);
+
+
+    }
 }
---
+++ lib/Models/SongModel.dart
@@ -0,0 +1,7 @@
+
+class SongModel{
+  String name;
+  String path;
+
+  SongModel(this.name,this.path);
+}
